3615525c0486bf2671dd54293b97dd3b606a7f60
Sameer Soin <sameer.soin@arm.com>
2019-09-12 19:42:15 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
+app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads/"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/encryption_uploader', methods = ['GET', 'POST'])
+def encryption_uploader():
+    if request.method == 'POST':
+        cover = request.files['cover']
+        secret = request.files['secret']
+        cover.save(os.path.join(app.config['UPLOAD_FOLDER'], "cover.png"))
+        secret.save(os.path.join(app.config['UPLOAD_FOLDER'], "secret.png"))
+        return "Cover and Secret Uploaded Succesfully"
+
+@app.route('/decryption_uploader', methods = ['GET', 'POST'])
+def decryption_uploader():
+    if request.method == 'POST':
+        stego = request.files['stego']
+        stego.save(os.path.join(app.config['UPLOAD_FOLDER'], "stego.png"))
+        return "Stego Uploaded Succesfully"
+app.run()
